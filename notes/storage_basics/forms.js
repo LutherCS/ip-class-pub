b9dfa023e7165052bad1b493874f53e263b17910
Roman Yasinovskyy <yasiro01@luther.edu>
2018-10-04 13:15:13 -0500

    Add local storage example

--- notes/storage_basics/forms.js
+++ notes/storage_basics/forms.js
@@ -4,19 +4,50 @@
 
 
 function populateSelect(selectId, selectValues) {
-
+    let sel = document.getElementById(selectId, selectValues);
+    for (let s of selectValues) {
+        let opt = document.createElement("option");
+        opt.value = s;
+        opt.innerHTML = s;
+        sel.appendChild(opt);
+    }
 }
 
 function addMeal() {
-
+    let menu = localStorage.getItem('menu');
+    menu = menu ? JSON.parse(menu) : [];
+    let selNames = ['quantity', 'name', 'meal'];
+    let newMeal = {};
+    for (let cid of selNames) {
+        newMeal[cid] = document.getElementById('sel_' + cid).value;
+    }
+    menu.push(newMeal);
+    localStorage.setItem('menu', JSON.stringify(menu));
 }
 
 function clearMenu() {
-
+    localStorage.removeItem('menu');
 }
 
 function loadMeal() {
+    let vals = JSON.parse(localStorage.getItem('menu'));
+    let menu = document.querySelector('#menu');
 
+    if (vals) {
+        for (let meal of vals) {
+            let menuItem = document.createElement('div');
+            menuItem.classList.add('alert');
+            menuItem.classList.add('alert-primary');
+            menuItem.innerHTML += `You are having ${meal['quantity']} ${meal['name']} for ${meal['meal']}`;
+            menu.appendChild(menuItem);
+        }
+    } else {
+        let banner = document.createElement('div');
+        banner.classList.add('alert');
+        banner.classList.add('alert-info');
+        banner.innerHTML = 'Please select from the menu';
+        menu.appendChild(banner);
+    }
 }
 
 $(document).ready(function () {
