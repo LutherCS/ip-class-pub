e1b32a84d403b10ed7894cb730f520918130a4ff
Roman Yasinovskyy <yasiro01@luther.edu>
2018-11-15 14:16:57 -0600

    Add class notes

--- notes/js_frontend/hello_vue.js
+++ notes/js_frontend/hello_vue.js
@@ -5,6 +5,24 @@
 const app = new Vue({
     el: "#app",
     data: {
-        message: "Hello from Vue"
+        message: "Fareway stock management"
     }
-})
+});
+
+const demo = new Vue({
+    el: "#store-demo",
+    data: {
+        products: [
+            {name: "Apple", price: 7.00.toFixed(2), quantity: 20 },
+            {name: "Banana", price: 0.11.toFixed(2), quantity: 2 },
+            {name: "Cheerios", price: 4.99.toFixed(2), quantity: 8 },
+        ]
+    },
+    computed: {
+        stock() {
+            return this.products.reduce((sum, product) => {
+                return sum + product.quantity;
+            }, 0)
+        }
+    }
+});
