ebc681d227b3acd201d5e958d1bf6ab0710b8b51
Roman Yasinovskyy <yasiro01@luther.edu>
2018-11-17 13:11:13 -0600

    Update Vue notes

--- notes/js_frontend/hello_vue.js
+++ notes/js_frontend/hello_vue.js
@@ -5,7 +5,7 @@
 const app = new Vue({
     el: "#app",
     data: {
-        message: "Fareway stock management"
+        message: "FarFar away stock management"
     }
 });
 
@@ -13,15 +13,18 @@ const demo = new Vue({
     el: "#store-demo",
     data: {
         products: [
-            {name: "Apple", price: 7.00.toFixed(2), quantity: 20 },
-            {name: "Banana", price: 0.11.toFixed(2), quantity: 2 },
-            {name: "Cheerios", price: 4.99.toFixed(2), quantity: 8 },
+            {name: "Apple", price: 7.00, quantity: 20 },
+            {name: "Banana", price: 0.11, quantity: 2 },
+            {name: "Cheerios", price: 4.99, quantity: 8 },
         ]
     },
     computed: {
         stock() {
             return this.products.reduce((sum, product) => {
-                return sum + product.quantity;
+                if (product.quantity > 0)
+                    return sum + product.quantity;
+                else
+                    return sum;
             }, 0)
         }
     }
