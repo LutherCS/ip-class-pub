303763619307237a0f674a7fa36d24030a170368
Roman Yasinovskyy <yasiro01@luther.edu>
2019-02-18 16:08:47 -0600

    Clean up for the new semester

--- notes/js_events/prime_table_btn.js
+++
@@ -1,8 +0,0 @@
-/* jshint esversion: 6 */
-/* jshint node: true */
-"use strict";
-
-function clickedon() {
-    let n = parseInt(document.querySelector('#numprimes').value);
-    generateTableOfPrimes('#primetbl', n);
-}
--- notes/js_events/prime_table_param.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 6 */
-/* jshint node: true */
-"use strict";
-
-$(document).ready(function () {
-    let all_params = new URLSearchParams(window.location.search);
-    let n = parseInt(all_params.get('n'));
-    generateTableOfPrimes('#primetbl', n);
-});
