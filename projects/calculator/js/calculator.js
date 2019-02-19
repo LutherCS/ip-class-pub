12a31d91e3f260ad44b28a6c7dbf2c2a11b43020
Roman Yasinovskyy <yasiro01@luther.edu>
2019-02-19 10:49:26 -0600

    Add Calculator project

---
+++ projects/calculator/js/calculator.js
@@ -0,0 +1,20 @@
+"use strict;"
+
+var screen;
+
+function enterDigit(digit) {
+}
+
+function clear_screen() {
+}
+
+function eval_expr() {
+}
+
+function enterOp(operation) {
+}
+
+$(document).ready(function () {
+    screen = document.querySelector("#res");
+    screen.innerHTML = "0";
+});
