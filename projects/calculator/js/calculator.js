16c91bbd5148fc42919a21936cfa0d845a44ec0a
Roman Yasinovskyy <yasiro01@luther.edu>
2019-09-12 11:14:45 -0500

    Add calculator project

---
+++ projects/calculator/js/calculator.js
@@ -0,0 +1,11 @@
+/* jshint esversion: 6 */
+/* jshint node: true */
+/* jshint browser: true */
+/* jshint jquery: true */
+'use strict';
+
+
+/* Executed when the document is ready (window is loaded) */
+$(document).ready(function () {
+
+});
