ba9810cfa23f0bed532e295f0bb5a4efb5c99951
Roman Yasinovskyy <yasiro01@luther.edu>
2019-09-19 12:44:12 -0500

    Add notes

---
+++ notes/bootstrap_input/js/script.js
@@ -0,0 +1,60 @@
+/* jshint esversion: 6 */
+/* jshint browser: true */
+"use strict;";
+
+var allCourses = {
+    "Fundamentals of Web Programming": [130, 2],
+    "Data Modeling and Querying": [140, 2],
+    "Introduction to Computer Science": [150, 4],
+    "Algorithms and Data Structures": [160, 4],
+    "Object-Oriented Programming With Java": [252, 2],
+    "Object-Oriented Programming With C++": [253, 2],
+    "Computational Models": [260, 4],
+    "Writing in the Major Lab": [296, 1],
+    "Internet Programming": [330, 4],
+    "Embedded Android Programming": [352, 2],
+    "Embedded iOS Programming": [353, 2],
+    "Advanced Algorithms and Data Structures": [360, 4],
+    "Programming Languages": [370, 4],
+    "Understanding Entrepreneurship in Silicon Valley": [385, 4],
+    "The Digital Transformation of Central Europe": [386, 4],
+    "Computer Networks": [430, 4],
+    "Database Management Systems": [440, 4],
+    "Operating Systems and Architecture": [450, 4],
+    "Information Assurance and Security": [460, 4],
+    "Senior Project I": [490, 2],
+    "Senior Project II": [491, 2]
+};
+
+var gradePoints = {
+    "A": 4.0,
+    "A-": 3.7,
+    "B+": 3.3,
+    "B": 3.0,
+    "B-": 2.7,
+    "C+": 2.3,
+    "C": 2.0,
+    "C-": 1.7,
+    "D+": 1.3,
+    "D": 1.0,
+    "D-": 0.7,
+    "F": 0.0
+};
+
+function populateTitleOptions(titleSelect) {
+}
+
+function populateGradeOptions(gradeSelect) {
+}
+
+function calculateGpa(buttonPressed=false) {
+}
+
+function updateCredits(courseId, courseTitle) {
+}
+
+function addCourse() {
+}
+
+$(document).ready(function () {
+});
