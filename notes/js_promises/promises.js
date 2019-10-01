5c4c73b5056145e43d94a9ba0cc6585465a9d712
Roman Yasinovskyy <yasiro01@luther.edu>
2019-10-01 14:17:35 -0500

    Update Promises notes

--- notes/js_promises/promises.js
+++ notes/js_promises/promises.js
@@ -1,3 +1,19 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 /* jshint node: true */
 'use strict';
+
+function resolve_with_wait() {
+    return new Promise( resolve => {
+        setTimeout(() => {
+            resolve("resolved");
+        }, 1000);
+    });
+}
+
+async function f() {
+    console.log("Calling resolve_with_wait");
+    var result = await resolve_with_wait();
+    console.log(result);
+}
+
+// f();
--- notes/js_promises/script.js
+++ notes/js_promises/script.js
@@ -1,13 +1,34 @@
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 /* jshint node: true */
 /* jshint browser: true */
 /* jshint jquery: true */
 'use strict';
 
-function populate() {
-//https://jsonplaceholder.typicode.com/posts
-//https://jsonplaceholder.typicode.com/users
-//https://jsonplaceholder.typicode.com/comments
+async function populate() {
+    // let data = await getData("https://jsonplaceholder.typicode.com/posts");
+    // console.log(data);
+    let [posts, users, comments] = await Promise.all([
+        getData("https://jsonplaceholder.typicode.com/posts"),
+        getData("https://jsonplaceholder.typicode.com/users"),
+        getData("https://jsonplaceholder.typicode.com/comments"),
+    ]);
+
+    console.log(posts);
+    // console.log(users);
+    // console.log(comments);
+    let postsDiv = document.querySelector("#posts");
+    for (let post of posts) {
+        let p = document.createElement("p");
+        p.innerText = post.body;
+        postsDiv.appendChild(p);
+
+    }
+}
+
+async function getData(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .catch(error => console.log(error));
 }
 
 $(document).ready(function () {
