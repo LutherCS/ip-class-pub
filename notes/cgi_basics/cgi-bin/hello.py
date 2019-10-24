2a8d10282a073403b00affde1cd092727c5d116f
Roman Yasinovskyy <yasiro01@luther.edu>
2019-10-24 13:16:29 -0500

    Add CGI example

---
+++ notes/cgi_basics/cgi-bin/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+
+print("Content-Type: text/html")
+print()
+print("<html>")
+print("<title>CS330</title>")
+print("<body>")
+print("Hello there")
+print(f"{random.randint(100, 200)}")
+print("</body>")
+print("</html>")
