b0d60534edfb630f9594678bc82026623e7776ea
Roman Yasinovskyy <yasiro01@luther.edu>
2019-10-29 13:55:55 -0500

    Update notes

--- notes/flask_methods/flask_methods.py
+++ notes/flask_methods/flask_methods.py
@@ -13,4 +13,28 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_user():
-    pass
+    if request.method == "GET":
+        user_name = request.cookies.get("username")
+        if user_name:
+            return render_template("index.html", username=user_name)
+        else:
+            return render_template("login.html")
+    else:  # POST
+        response = make_response(redirect(url_for("hello_user"), code=303))
+
+        if request.form.get("logout"):
+            response.set_cookie("username", "", expires=0)
+        else:
+            response.set_cookie("username", request.form.get("username"))
+
+        return response
+
+
+@app.route("/css/<path:some_path>")
+def serve_css(some_path):
+    return send_from_directory("static/css", some_path)
+
+
+@app.route("/js/<path:some_path>")
+def serve_js(some_path):
+    return send_from_directory("static/js", some_path)
