cc33c3eb2f1ea6cf65756ae99af36b441fe18319
Roman Yasinovskyy <yasiro01@luther.edu>
2019-10-29 13:24:20 -0500

    Complete templates notes

--- notes/flask_templates/app.py
+++ notes/flask_templates/app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/hello/<string:name>")
-def hello(name=None):
-    return render_template("index.html", name=name)
+@app.route("/hello/<string:name_param>")
+def hello(name_param=None):
+    return render_template("index.html", name=name_param)
+
+
+@app.route("/<int:n>")
+def get_numbers(n):
+    return render_template("number.html", nums=list(range(n)))
