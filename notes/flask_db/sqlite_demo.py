c584370132954b07802792a83df14b81d180ce41
Roman Yasinovskyy <yasiro01@luther.edu>
2019-11-05 13:34:18 -0600

    Update sqlite notes

--- notes/flask_db/sqlite_demo.py
+++ notes/flask_db/sqlite_demo.py
@@ -17,7 +17,34 @@ def read_txt(filename):
 def main(args):
     """Main function"""
     operation = args[1]
-    pass
+    conn = sqlite3.connect("zoo.sqlite3")
+    cur = conn.cursor()
+    if operation == "create":
+        cur.execute("DROP TABLE IF EXISTS animal;")
+        cur.execute("CREATE TABLE animal (id, name, age, species, location);")
+        animals = read_txt("zoo.txt")
+        cur.executemany("INSERT INTO animal VALUES(?, ?, ?, ?, ?);", animals)
+        conn.commit()
+    elif operation == "read":
+        if len(args) > 2:
+            species = (args[2], )
+            cur.execute("SELECT * FROM animal WHERE species=?;", species)
+        else:
+            cur.execute("SELECT * FROM animal;")
+        print("RESULTS")
+        for animal in cur:
+            print(animal)
+    elif operation == "update":
+        cur.execute("UPDATE animal SET age = age + 1;")
+        conn.commit()
+    elif operation == "delete":
+        if len(args) > 2:
+            species = (args[2], )
+            cur.execute("DELETE FROM animal WHERE species=?;", species)
+        else:
+            cur.execute("DELETE FROM animal;")
+        conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
