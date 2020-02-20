8fa97697cbdb066e93cf5278beafb7ba68545ac6
Roman Yasinovskyy <yasiro01@luther.edu>
2020-02-20 16:58:12 -0600

    Update class notes

--- notes/class_5/js/script.js
+++ notes/class_5/js/script.js
@@ -37,39 +37,52 @@ function populateGradeOption(gradeSelect) {
 
 function calculateGPA() {
     let alert_box = document.querySelector("#thegpa");
-    alert_box.innerHTML = "you clicked the button";
+    alert_box.innerHTML = "calculating your gpa. please wait <img src='img/22.gif' height='10px'>";
 }
 
 function help() {
     let alert_box = document.querySelector("#thegpa");
-    alert_box.innerHTML = "See notes/bootstrap_input for complete example";
+    alert_box.innerHTML = "See notes/bootstrap_input/ for complete example";
 }
 
 function addGrade() {
     let allMyCourses = document.querySelector("#grades");
-    let currentCourse = document.querySelectorAll("#grades > [class='row']").length + 1;
+    let currentCourseId = document.querySelectorAll("#grades > [class='row']").length + 1;
     let thisCourseDiv = document.createElement("div");
     thisCourseDiv.setAttribute("class", "row");
 
     let titleDiv = document.createElement("div");
-    titleDiv.setAttribute("class", "form-group-col");
+    titleDiv.setAttribute("class", "form-group col-6");
+    thisCourseDiv.appendChild(titleDiv);
 
     let titleSelect = document.createElement("select");
     titleSelect.setAttribute("class", "form-control select_title");
-    titleSelect.setAttribute("id", `title${currentCourse}`);
+    titleSelect.setAttribute("id", `title${currentCourseId}`);
+    populateTitleOption(titleSelect);
+    titleDiv.appendChild(titleSelect);
 
     let creditsDiv = document.createElement("div");
     creditsDiv.setAttribute("class", "form-group col-3");
+    thisCourseDiv.appendChild(creditsDiv);
 
     let creditsInput = document.createElement("input");
     creditsInput.setAttribute("type", "number");
+    creditsInput.setAttribute("class", "form-control input_credits");
+    creditsInput.setAttribute("id", `credits${currentCourseId}`);
     creditsInput.setAttribute("min", 0);
-
     creditsDiv.appendChild(creditsInput);
 
-    populateTitleOption(titleSelect);
-    titleDiv.appendChild(titleSelect);
-    thisCourseDiv.appendChild(titleDiv);
-    thisCourseDiv.appendChild(creditsDiv);
+    let gradeDiv = document.createElement("div");
+    gradeDiv.setAttribute("class", "form-group col-3");
+    thisCourseDiv.appendChild(gradeDiv);
+
+    let gradeSelect = document.createElement("select");
+    gradeSelect.setAttribute("class", "form-control select_grade");
+    gradeSelect.setAttribute("id", `grade${currentCourseId}`);
+    gradeSelect.setAttribute("onchange", "calculateGpa()");
+    populateGradeOption(gradeSelect);
+    gradeDiv.appendChild(gradeSelect);
+
+    
     allMyCourses.appendChild(thisCourseDiv);
 }
