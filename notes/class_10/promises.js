65542c228d0cf1465e617584280ce5037a6a1309
Roman Yasinovskyy <yasiro01@luther.edu>
2020-03-10 08:31:30 -0500

    Add Promises example

---
+++ notes/class_10/promises.js
@@ -0,0 +1,28 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+'use strict';
+
+function resolve_with_wait() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve("resolved");
+    }, 1000);
+    });
+}
+
+function func() {
+    console.log("Calling resolve_with_timeout");
+    let result = resolve_with_wait();
+    console.log(result);
+}
+
+// func();
+
+async function async_func() {
+    console.log("Calling resolve_with_wait");
+    let result = await resolve_with_wait();
+    console.log(result);
+    console.log("Done")
+}
+
+async_func();
