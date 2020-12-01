db1a234317e543cc9b1d45dc3070663133cb4412
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-11-30 22:55:06 -0600

    Add Exercise Primes

---
+++ exercises/primes/primes.js
@@ -0,0 +1,26 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+'use strict';
+
+var urlParams = new URLSearchParams(window.location.search);
+
+function greet() {
+}
+
+function isPrime(n) {
+}
+
+function printPrimeNumber() {
+}
+
+function getNPrimes(n) {
+}
+
+function printNPrimes() {
+}
+
+window.onload = function() {
+    greet();
+    printPrimeNumber();
+    printNPrimes();
+};
