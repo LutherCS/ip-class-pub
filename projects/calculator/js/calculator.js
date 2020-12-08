0fbfa90eb70029a715c8d439ac75e604314b9c28
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-12-07 23:37:04 -0600

    Add project 1: Calculator

---
+++ projects/calculator/js/calculator.js
@@ -0,0 +1,24 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+/* jshint browser: true */
+'use strict';
+
+var screen;
+
+
+function enterDigit(digit) {
+}
+
+function clear_screen() {
+}
+
+function eval_expr() {
+}
+
+function enterOp(operation) {
+}
+
+window.onload = function () {
+    screen = document.querySelector("#result");
+    screen.innerHTML = "0";
+};
