41542523b0f5bc71a1ac455df79d2b91e9458dfa
Roman Yasinovskyy <yasinovskyy@gmail.com>
2020-12-15 02:25:28 -0600

    Add Promises exercise

---
+++ exercises/promises/js/script.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 8 */
+/* jshint browser: true */
+/* jshint node: true */
+'use strict';
+
+async function get_individual(num, all_numbers) {
+
+}
+
+async function get_batch(num, all_numbers) {
+
+}
+
+async function clickedon() {
+    let num = parseInt(document.querySelector('#number').value);
+    let all_numbers = document.querySelector('#number_info');
+    if (document.querySelector('#batch').checked) {
+        get_batch(num, all_numbers);
+    } else {
+        get_individual(num, all_numbers);
+    }
+}
