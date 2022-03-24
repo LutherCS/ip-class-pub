beaea23e7d3f4218e9e608845fe0d41a329f6e5d
Roman Yasinovskyy <yasinovskyy@gmail.com>
2022-03-24 18:19:05 -0500

    Update tests

--- tests/primes/test_primes.py
+++ tests/primes/test_primes.py
@@ -2,7 +2,9 @@
 """Testing JavaScript"""
 
 import subprocess
-from time import sleep
+
+import pytest
+from playwright.sync_api import Page
 
 
 def setup_module(module):
@@ -19,61 +21,69 @@ def teardown_module(module):
     module.http_server.terminate()
 
 
-def test_case_1_greeting(page):
-    page.goto("http://localhost:8000/primes.html?name=Roman&n=3")
-    assert page.querySelector("#greeting").innerText() == "Hello Roman"
+def test_case_1_greeting(page: Page):
+    page.goto("http://localhost:8000/primes.html?name=Roman&number=3")
+    assert page.query_selector("#greeting").inner_text() == "Hello Roman"
 
 
-def test_case_1_n(page):
-    page.goto("http://localhost:8000/primes.html?name=Roman&n=3")
-    assert page.querySelector("#primeInfo").innerText() == "3 is a prime number"
+def test_case_1_number(page: Page):
+    page.goto("http://localhost:8000/primes.html?name=Roman&number=3")
+    assert page.query_selector("#numberInfo").inner_text() == "3 is a prime number"
 
 
-def test_case_1_table(page):
-    page.goto("http://localhost:8000/primes.html?name=Roman&n=3")
-    assert len(page.querySelectorAll("table[id='nPrimes'] > tbody > tr")) == 3
+def test_case_1_table(page: Page):
+    page.goto("http://localhost:8000/primes.html?name=Roman&number=3")
+    assert len(page.query_selector_all("table[id='nPrimes'] > tbody > tr")) == 3
 
 
-def test_case_2_greeting(page):
+def test_case_2_greeting(page: Page):
     page.goto("http://localhost:8000/primes.html?name=Roman")
-    assert page.querySelector("#greeting").innerText() == "Hello Roman"
+    assert page.query_selector("#greeting").inner_text() == "Hello Roman"
 
 
-def test_case_2_n(page):
+def test_case_2_number(page: Page):
     page.goto("http://localhost:8000/primes.html?name=Roman")
-    assert page.querySelector("#primeInfo").innerText() == "330 is not a prime number"
+    assert (
+        page.query_selector("#numberInfo").inner_text() == "330 is not a prime number"
+    )
 
 
-def test_case_2_table(page):
+def test_case_2_table(page: Page):
     page.goto("http://localhost:8000/primes.html?name=Roman")
-    assert len(page.querySelectorAll("table[id='nPrimes'] > tbody > tr")) == 330
+    assert len(page.query_selector_all("table[id='nPrimes'] > tbody > tr")) == 330
 
 
-def test_case_3_greeting(page):
-    page.goto("http://localhost:8000/primes.html?n=3")
-    assert page.querySelector("#greeting").innerText() == "Hello student"
+def test_case_3_greeting(page: Page):
+    page.goto("http://localhost:8000/primes.html?number=3")
+    assert page.query_selector("#greeting").inner_text() == "Hello student"
 
 
-def test_case_3_n(page):
-    page.goto("http://localhost:8000/primes.html?n=3")
-    assert page.querySelector("#primeInfo").innerText() == "3 is a prime number"
+def test_case_3_number(page: Page):
+    page.goto("http://localhost:8000/primes.html?number=3")
+    assert page.query_selector("#numberInfo").inner_text() == "3 is a prime number"
 
 
-def test_case_3_table(page):
-    page.goto("http://localhost:8000/primes.html?n=3")
-    assert len(page.querySelectorAll("table[id='nPrimes'] > tbody > tr")) == 3
+def test_case_3_table(page: Page):
+    page.goto("http://localhost:8000/primes.html?number=3")
+    assert len(page.query_selector_all("table[id='nPrimes'] > tbody > tr")) == 3
 
 
-def test_case_4_greeting(page):
+def test_case_4_greeting(page: Page):
     page.goto("http://localhost:8000/primes.html")
-    assert page.querySelector("#greeting").innerText() == "Hello student"
+    assert page.query_selector("#greeting").inner_text() == "Hello student"
 
 
-def test_case_4_n(page):
+def test_case_4_number(page: Page):
     page.goto("http://localhost:8000/primes.html")
-    assert page.querySelector("#primeInfo").innerText() == "330 is not a prime number"
+    assert (
+        page.query_selector("#numberInfo").inner_text() == "330 is not a prime number"
+    )
 
 
-def test_case_4_table(page):
+def test_case_4_table(page: Page):
     page.goto("http://localhost:8000/primes.html")
-    assert len(page.querySelectorAll("table[id='nPrimes'] > tbody > tr")) == 330
+    assert len(page.query_selector_all("table[id='nPrimes'] > tbody > tr")) == 330
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
