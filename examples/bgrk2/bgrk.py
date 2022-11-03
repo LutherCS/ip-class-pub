5b77a2aeb8e7d258bf467550226247c3d681dcbf
Roman Yasinovskyy <yasinovskyy@gmail.com>
2022-11-03 14:23:44 -0500

    Update BGRK example

--- examples/bgrk2/bgrk.py
+++ examples/bgrk2/bgrk.py
@@ -3,59 +3,76 @@
 Board Game Record Keeper
 
 @author: Roman
-@version: 2022.10
+@version: 2022.11
 """
 
 from csv import DictReader
+from functools import cache
+
+from dotenv import load_dotenv
 from flask import (
     Flask,
+    jsonify,
     make_response,
     redirect,
     render_template,
     request,
+    session,
     url_for,
-    jsonify,
 )
 
-app = Flask(__name__)
 
-all_games = dict()
-chosen_games = dict()
+@cache
+def read_data_file(filename):
+    all_games = {}
+    with open(filename) as datafile:
+        for record in DictReader(datafile):
+            all_games[record["title"]] = record
+    return all_games
+
+
+def create_app():
+    app = Flask(__name__)
+    load_dotenv()
+    app.config.from_prefixed_env()
+    app.config["all_games"] = read_data_file("data/games.csv")
+
+    return app
+
+
+app = create_app()
 
 
 @app.get("/")
 def index():
-    global all_games
-    global chosen_games
-    if not all_games:
-        with open("data/games.csv") as datafile:
-            for record in DictReader(datafile):
-                all_games[record["title"]] = record
+    all_games = app.config["all_games"]
+    chosen_games = session.get("chosen_games", [])
     if not chosen_games:
         return render_template("base.html", games=all_games.values())
     return render_template(
-        "games.html", games=all_games.values(), collection=chosen_games.values()
+        "games.html", games=all_games.values(), collection=chosen_games
     )
 
 
 @app.post("/addgame")
 def read_user_selection():
-    global all_games
-    global chosen_games
+    all_games = app.config["all_games"]
     response = make_response(redirect(url_for("index"), code=303))
     game_title = request.form.get("game")
-    chosen_games[game_title] = all_games[game_title]
+    chosen_games = session.get("chosen_games", [])
+    chosen_games.append(all_games[game_title])
+    session["chosen_games"] = chosen_games
 
     return response
 
 
 @app.get("/api/v1/games/all")
 def get_all_games():
-    global all_games
-    return jsonify(all_games)
+    all_games = app.config["all_games"]
+    return all_games
 
 
 @app.get("/api/v1/games/my")
 def get_user_games():
-    global chosen_games
+    chosen_games = session.get("chosen_games", [])
     return jsonify(chosen_games)
