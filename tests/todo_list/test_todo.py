f6c0e4d57ac4615aa0a89652a6b0d48c6338cd01
Roman Yasinovskyy <yasinovskyy@gmail.com>
2023-09-19 10:40:01 -0500

    Update todo_list tests

--- tests/todo_list/test_todo.py
+++ tests/todo_list/test_todo.py
@@ -3,11 +3,10 @@
 `todo_list` testing
 
 @authors: Roman Yasinovskyy
-@version: 2022.9
+@version: 2023.9
 """
 
 import subprocess
-from time import sleep
 
 import pytest
 from playwright.sync_api import Page
@@ -16,6 +15,7 @@ TIMEOUT = 1000
 
 
 def setup_module(module):
+    """Set up"""
     module.http_server = subprocess.Popen(
         ["python3", "-m", "http.server", "--directory", "exercises/todo_list"]
     )
@@ -26,46 +26,48 @@ def setup_module(module):
 
 
 def teardown_module(module):
+    """Tear down"""
     module.http_server.terminate()
 
 
 def test_no_input(page: Page):
+    """Button clicked without any input"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.click("#addTaskBtn")
     assert (
-        page.query_selector("#feedbackMessage").inner_text()
-        == "Fill out title and due date"
+        page.locator("#feedbackMessage").inner_text() == "Fill out title and due date"
     )
 
 
 def test_no_date(page: Page):
+    """Date not chosen"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.fill("#title", "Task title")
     page.click("#addTaskBtn")
     assert (
-        page.query_selector("#feedbackMessage").inner_text()
-        == "Fill out title and due date"
+        page.locator("#feedbackMessage").inner_text() == "Fill out title and due date"
     )
 
 
 def test_no_title(page: Page):
+    """Title not chosen"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
-    page.fill("#dueDate", "2022-09-25")
+    page.fill("#dueDate", "2023-09-26")
     page.click("#addTaskBtn")
     assert (
-        page.query_selector("#feedbackMessage").inner_text()
-        == "Fill out title and due date"
+        page.locator("#feedbackMessage").inner_text() == "Fill out title and due date"
     )
 
 
 def test_no_selection(page: Page):
+    """Default selection"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.fill("#title", "Task title")
-    page.fill("#dueDate", "2022-09-25")
+    page.fill("#dueDate", "2023-09-26")
     page.click("#addTaskBtn")
     all_rows = page.query_selector_all("table[id='taskList'] > tbody > tr")
     new_row = all_rows[-1]
@@ -73,7 +75,7 @@ def test_no_selection(page: Page):
     assert new_row.query_selector_all("td")[1].inner_text() == "Task title"
     assert new_row.query_selector_all("td")[2].inner_text() == "Aardvark"
     assert new_row.query_selector_all("td")[3].inner_text() == "Low"
-    assert new_row.query_selector_all("td")[4].inner_text() == "2022-09-25"
+    assert new_row.query_selector_all("td")[4].inner_text() == "2023-09-26"
 
 
 @pytest.mark.parametrize(
@@ -89,11 +91,12 @@ def test_no_selection(page: Page):
         "Hippo",
     ],
 )
-def test_select_worker(page: Page, worker):
+def test_select_worker(page: Page, worker: str):
+    """Various workers"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.fill("#title", "Task title")
-    page.fill("#dueDate", "2022-09-25")
+    page.fill("#dueDate", "2023-09-26")
     page.select_option("#assignedTo", worker)
     page.click("#addTaskBtn")
     new_row = page.query_selector_all("table[id='taskList'] > tbody > tr")[-1]
@@ -102,29 +105,31 @@ def test_select_worker(page: Page, worker):
 
 
 @pytest.mark.parametrize("priority", ["Low", "Normal", "Important", "Critical"])
-def test_select_priority(page: Page, priority):
+def test_select_priority(page: Page, priority: str):
+    """Various priorities"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.fill("#title", "Task title")
-    page.fill("#dueDate", "2022-09-25")
+    page.fill("#dueDate", "2023-09-26")
     page.select_option("#priority", priority)
     page.click("#addTaskBtn")
     new_row = page.query_selector_all("table[id='taskList'] > tbody > tr")[-1]
 
     assert new_row.query_selector_all("td")[3].inner_text() == priority
-    assert priority.lower() in new_row.get_attribute("class")
+    assert new_row.get_attribute("class").index(priority.lower()) > -1
 
 
 def test_remove_row(page: Page):
+    """Remove a task"""
     page.set_default_timeout(TIMEOUT)
     page.goto("http://localhost:8000/")
     page.fill("#title", "Task title")
-    page.fill("#dueDate", "2022-09-25")
+    page.fill("#dueDate", "2023-09-26")
     page.click("#addTaskBtn")
     all_rows = page.query_selector_all("table[id='taskList'] > tbody > tr")
     assert len(all_rows) == 1
-    page.check("table[id='taskList'] > tbody > tr > td > input[type='checkbox']")
-    sleep(3)
+    page.click("table[id='taskList'] > tbody > tr > td > input[type='checkbox']")
+    page.wait_for_timeout(3000)
     all_rows = page.query_selector_all("table[id='taskList'] > tbody > tr")
     assert len(all_rows) == 0
 
