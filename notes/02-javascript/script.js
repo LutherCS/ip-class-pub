8689f3eeafc6c5e8e1628fc259b9c09e7be693da
Roman Yasinovskyy <yasinovskyy@gmail.com>
2024-09-12 14:04:49 -0500

    Add JS notes

---
+++ notes/02-javascript/script.js
@@ -0,0 +1,47 @@
+// console.log("hello")
+
+// Read about var, const, let, and global variables somewhere
+// let name = "Ramesh"
+// console.log(name)
+// console.log(firstName)
+// var firstName = "Alex"
+// console.log(firstName)
+
+let roster = ["Mughees", "Joe", 4, 8, 42, true]
+// console.log(roster)
+// roster.sort()
+// console.log(roster)
+
+// for (let name in roster) {
+//     console.log(name);
+// }
+
+// for (let name of roster) {
+//     console.log(name);
+// }
+
+// for (let idx in roster) {
+//     console.log(idx, roster[idx]);
+// }
+
+// console.log(roster)
+// roster.push(false)
+// roster.sort()
+// console.log(roster)
+
+let grades = {"Alex": 6, "Bobby": 3}
+// console.log(grades)
+
+// for (smt in grades) {
+//     console.log(smt)
+// }
+
+// for (smt of grades) {
+//     console.log(smt)
+// }
+
+function helloWorld(name) {
+    return `Hello, ${name}`;
+}
+
+console.log(helloWorld("Daniel"))
